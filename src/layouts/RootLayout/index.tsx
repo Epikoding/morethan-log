7fd1d997e78ad4f73ccbb3274726774f0cbda303
Don-torder <ldj1@torder.co.kr>
2024-01-10 11:08:34 +0900

    스크롤 바 기능 revert

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -1,64 +1,25 @@
-import React, { useState, useEffect, useRef, ReactNode } from "react"
+import React, { ReactNode } from "react"
 import { ThemeProvider } from "./ThemeProvider"
 import useScheme from "src/hooks/useScheme"
 import Header from "./Header"
 import styled from "@emotion/styled"
 import Scripts from "src/layouts/RootLayout/Scripts"
 import useGtagEffect from "./useGtagEffect"
-import useThrottle from "src/hooks/useThrottle"
-import { useRouter } from "next/router"
 
 type Props = {
   children: ReactNode
 }
 
 const RootLayout = ({ children }: Props) => {
-  const router = useRouter()
-  const currentElementRef = useRef<HTMLDivElement | null>(null)
-
-  const [blogHeight, setBlogHeight] = useState(0)
-  const [throttleScrollY, setThrottleScrollY] = useState<number>(0)
   const [scheme] = useScheme()
-  console.log(throttleScrollY)
   useGtagEffect()
-
-  const scrollThrottle = useThrottle(() => {
-    setThrottleScrollY(window.scrollY)
-  }, 100)
-
-  const getCurrentPercentage = () => {
-    if (router.asPath === "/") return 0
-
-    let percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
-
-    if (percentage >= 90) {
-      percentage = 100
-    } else {
-      percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
-    }
-    return percentage
-  }
-
-  useEffect(() => {
-    if (currentElementRef.current) {
-      const clientHeight = currentElementRef.current.clientHeight
-      setBlogHeight(clientHeight)
-    }
-
-    window.addEventListener("scroll", scrollThrottle)
-    return () => window.removeEventListener("scroll", scrollThrottle)
-  }, [throttleScrollY])
-
   return (
     <ThemeProvider scheme={scheme}>
       <Scripts />
       {/* // TODO: replace react query */}
       {/* {metaConfig.type !== "Paper" && <Header />} */}
-      <Header
-        fullWidth={false}
-        readingProgress={blogHeight < 1200 ? 0 : getCurrentPercentage()}
-      />
-      <StyledMain ref={currentElementRef}>{children}</StyledMain>
+      <Header fullWidth={false} />
+      <StyledMain>{children}</StyledMain>
     </ThemeProvider>
   )
 }
